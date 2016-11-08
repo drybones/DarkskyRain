d0be51bb2cc6d2dc937d617bdeb4f825096856c0
David Bick <david@drybones.co.uk>
2016-11-08 22:43:55 +0000

    Simple display of timeseries forecasts

--- src/DarkskyRain/wwwroot/darksky-rain.js
+++ src/DarkskyRain/wwwroot/darksky-rain.js
@@ -1,13 +1,53 @@
-﻿function ForecastSummaryModel() {
+﻿function RainDataPoint(item) {
     var self = this;
-    self.currently = ko.observable("[no forecast]"),
-    self.minutely = ko.observable("[no forecast]"),
-    self.hourly = ko.observable("[no forecast]")
+    self.timestamp = ko.observable(item.time);
+    self.probability = ko.observable(item.precipProbability);
+    self.intensity = ko.observable(item.precipIntensity);
+    self.error = ko.observable(item.precipIntensityError);
+
+    self.datetime = ko.computed(function () {
+        return new Date(self.timestamp() * 1000);
+    }, self);
+
+    self.time = ko.computed(function () {
+        var hours = self.datetime().getHours();
+        var minutes = "0" + self.datetime().getMinutes();
+        return hours + ':' + minutes.substr(-2);
+    }, this);
+}
+
+function ForecastSummaryModel() {
+    var self = this;
+    self.currentlySummary = ko.observable("[getting forecast]");
+    self.minutelySummary = ko.observable("[getting forecast]");
+    self.hourlySummary = ko.observable("[getting forecast]");
+
+    self.currentlyData = ko.observable();
+    self.minutelyData = ko.observableArray();
+    self.hourlyData = ko.observableArray();
 
     $.getJSON("/api/forecast/", function (forecastData) {
-        self.currently(forecastData.currently.summary);
-        self.minutely(forecastData.minutely.summary);
-        self.hourly(forecastData.hourly.summary);
+        self.currentlySummary(forecastData.currently.summary);
+        self.minutelySummary(forecastData.minutely.summary);
+        self.hourlySummary(forecastData.hourly.summary);
+
+        var currentlyDataItem = {
+            time: forecastData.currently.time,
+            precipProbability: forecastData.currently.precipProbability,
+            precipIntensity: forecastData.currently.precipIntensity,
+            precipIntensityError: forecastData.currently.precipIntensityError
+        };
+        self.currentlyData(new RainDataPoint(currentlyDataItem));
+
+        var mappedMinutelyData = $.map(forecastData.minutely.data, function (item) {
+            return new RainDataPoint(item);
+        })
+        self.minutelyData(mappedMinutelyData);
+
+        var mappedHourlyData = $.map(forecastData.hourly.data, function (item) {
+            return new RainDataPoint(item);
+        })
+        self.hourlyData(mappedHourlyData);
     });
 }
 
